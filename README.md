c6515aae89f185d2d342fb48955a646632830928
meera <meera@Meera>
2024-02-28 11:49:25 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Erpgold
+
+This is a app to information of gold in shop
+
+#### License
+
+MIT
---
+++ erpgold/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Erpgold",
+			"type": "module",
+			"label": _("Erpgold")
+		}
+	]
