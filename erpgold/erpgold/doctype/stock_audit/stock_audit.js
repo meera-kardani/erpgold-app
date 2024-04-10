403dd9151eb5409b981b004f609da51c15be7d69
meera-kardani <kardanimeera@gmail.com>
2024-04-10 18:46:00 +0530

    fix: fix issue of count in stock audit doctype

--- erpgold/erpgold/doctype/stock_audit/stock_audit.js
+++ erpgold/erpgold/doctype/stock_audit/stock_audit.js
@@ -8,6 +8,7 @@ frappe.ui.form.on('Stock Audit', {
                 if (response.message) {
                     var counts = response.message;
                     frm.set_value('total_items_in_stock', counts.in_stock);
+                    frm.set_value('total_not_found_items', counts.not_found)
                     frm.set_value('total_not_in_stock_items', counts.not_in_stock);
                     frm.refresh_field(['total_items_in_stock', 'total_not_in_stock_items']);
                 }
--- erpgold/erpgold/doctype/stock_audit/stock_audit.py
+++ erpgold/erpgold/doctype/stock_audit/stock_audit.py
@@ -8,17 +8,21 @@ class StockAudit(Document):
     def before_save(self):
         counts = self.get_count()
         self.total_items_in_stock = counts['in_stock']
+        self.total_not_found_items = counts['not_found']
         self.total_not_in_stock_items = counts['not_in_stock']
 
     @frappe.whitelist()
     def get_count(self):
-        total_items_in_stock = frappe.db.count('Serial No', filters={'purchase_document_type': 'Stock Entry'})
-        total_not_in_stock_items = frappe.db.count('Serial No', filters={'purchase_document_type': ['!=', 'Stock Entry']})
+        total_items_in_stock = frappe.db.count('Serial No', filters={'status': 'Active'})
+        total_not_found_items = frappe.db.count('Serial No', filters={'status':'Delivered'})
+        total_not_in_stock_items = frappe.db.count('Serial No', filters={'status':'Inactive'})
 
         print("Total Items in Stock:", total_items_in_stock)
+        print("Total not in stock items", total_not_found_items)
         print("Total not in stock items", total_not_in_stock_items)
 
         return {
             'in_stock': total_items_in_stock,
+            'not_found': total_not_found_items,
             'not_in_stock': total_not_in_stock_items
         }
