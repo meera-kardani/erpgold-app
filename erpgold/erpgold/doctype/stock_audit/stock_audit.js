aeadc117eaabf4aec2af367b624133c29dddc781
meera-kardani <kardanimeera@gmail.com>
2024-04-16 17:40:15 +0530

    fix:fix issue of count and check item in stock audit doctype

--- erpgold/erpgold/doctype/stock_audit/stock_audit.js
+++ erpgold/erpgold/doctype/stock_audit/stock_audit.js
@@ -7,9 +7,7 @@ frappe.ui.form.on('Stock Audit', {
                 if (response.message) {
                     var counts = response.message;
                     frm.set_value('total_items_in_stock', counts.in_stock);
-                    frm.set_value('total_not_found_items', counts.not_found);
-                    frm.set_value('total_not_in_stock_items', counts.not_in_stock);
-                    frm.refresh_field(['total_items_in_stock', 'total_not_found_items', 'total_not_in_stock_items']);
+                    frm.refresh_field(['total_items_in_stock']);
 
                     frm.script_manager.trigger("populate_stock_items");
                 }
@@ -46,28 +44,58 @@ frappe.ui.form.on('Stock Audit', {
             var exists = false;
             frm.doc.stock_items.forEach(function(item) {
                 if (item.serial_nos === scan_barcode) {
+                    if (item.available !== 1) {
+                        frappe.model.set_value(item.doctype ,item.name ,"available" , 1)
+                    }
                     exists = true;
                     return false;
                 }
             });
             if (!exists) {
-                frappe.db.get_value('Serial No', { name: scan_barcode }, 'item_code', function(response) {
+                frappe.db.get_value('Serial No', { name: scan_barcode }, ['item_code', 'status'], function (response) {
                     if (response && response.item_code) {
-                        var new_row = frm.add_child('not_in_stock');
-                        new_row.serial_nos = scan_barcode;
-                        new_row.item_code = response.item_code;
-                        frm.refresh_field('not_in_stock');
-                        frappe.msgprint('Serial number added to Not In Stock');
+                        if (response.status === 'Inactive') {
+                            // Check if the serial number is already in frm.inactive_serials
+                            if (!frm.inactive_serials || !frm.inactive_serials.includes(scan_barcode)) {
+                                frm.inactive_serials = frm.inactive_serials || [];
+                                frm.inactive_serials.push(scan_barcode);
+                                frm.set_value('total_not_found_items', (frm.doc.total_not_found_items || 0) + 1);
+                                frappe.msgprint('Serial number is inactive');
+                            } else {
+                                frappe.msgprint('Serial number is already marked as inactive');
+                            }
+                        } else if (response.status === 'Delivered') {
+                            // Check if the serial number is already in not_in_stock table
+                            var serialExistsInNotInStock = frm.doc.not_in_stock.some(function(item) {
+                                return item.serial_nos === scan_barcode;
+                            });
+                            if (!serialExistsInNotInStock) {
+                                var new_row = frm.add_child('not_in_stock');
+                                new_row.serial_nos = scan_barcode;
+                                new_row.item_code = response.item_code;
+                                frm.refresh_field('not_in_stock');
+                                frm.set_value('total_not_in_stock_items', frm.doc.not_in_stock.length);
+                                frappe.msgprint('Serial number added to Not In Stock');
+                            } else {
+                                frappe.msgprint('Serial number already exists in Not In Stock');
+                            }
+                        } else {
+                            frappe.msgprint('Serial number is not delivered');
+                        }
                     } else {
                         frappe.msgprint('Failed to fetch serial details');
                     }
                 });
-            } 
+            } else {
+                frappe.msgprint('Serial number already exist in stock item table');
+            }
         }
-        
+
         setTimeout(function() {
             frm.set_value('scan_barcode', '');
             frm.refresh_field('scan_barcode');
         }, 120);
+
+        console.log('Inactive Serials:', frm.inactive_serials);
     }
 });
